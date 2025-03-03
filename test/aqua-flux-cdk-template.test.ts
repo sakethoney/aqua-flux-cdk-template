312d18c82ae3e41517f1fff57b515a20a42c0fdd
sakethoney <saket.kumar.cs@gmail.com>
2025-03-02 19:22:29 -0500

    Initial commit

---
+++ test/aqua-flux-cdk-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AquaFluxCdkTemplate from '../lib/aqua-flux-cdk-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aqua-flux-cdk-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AquaFluxCdkTemplate.AquaFluxCdkTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
