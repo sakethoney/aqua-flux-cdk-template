ad9e8e919d5e4a059968234dc659af2ab30d456d
sakethoney <saket.kumar.cs@gmail.com>
2025-03-02 21:20:14 -0500

    vpc template

--- lib/aqua-flux-cdk-template-stack.ts
+++ lib/aqua-flux-cdk-template-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AquaFluxCdkTemplateStack extends cdk.Stack {
+
+  public readonly vpc: ec2.Vpc;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a VPC
+    this.vpc = new ec2.Vpc(this, 'MyAquaVPC', {
+      maxAzs: 1, // Maximum Availability Zones
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'), // Custom CIDR block
+      subnetConfiguration: [] // No subnets yet, we will add them in the next step
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AquaFluxCdkTemplateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Output VPC ID
+    new cdk.CfnOutput(this, 'VpcId', { value: this.vpc.vpcId });
   }
 }
